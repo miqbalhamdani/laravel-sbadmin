fd13b43a80c6dbe8bb3e4b0b1e9da83089254bd1
Iqbal Hamdani <iqbal.hamdani@misteraladin.com>
2020-02-23 05:47:58 +0700

    init template to laravel

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Dashboard - SB Admin</title>
+
+    @include('partial.head')
+  </head>
+
+  <body class="sb-nav-fixed">
+    @include('partial.nav')
+
+    <div id="layoutSidenav">
+      @include('partial.sidebar')
+
+      <div id="layoutSidenav_content">
+        <main>
+          <div class="container-fluid">
+            @yield('content')
+          </div>
+        </main>
+
+        @include('partial.footer')
+      </div>
+    </div>
+
+    @include('partial.script')
+  </body>
+</html>
